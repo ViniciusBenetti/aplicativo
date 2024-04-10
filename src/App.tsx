759529b2802bb9b973cba96241dfaa3c9c22857c
ViniciusBenetti <makako1289@gmail.com>
2024-04-10 18:14:29 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { Cadastro } from './components/Cadastro';
+import { EsqueceuSenha } from './components/EsqueceuSenha';
+import { Logou } from './components/Logou';
+import { Criadores } from './components/Criadores';
+import { Sobre } from './components/Sobre';
+import { BlogSemLogar } from './components/BlogSemLogar';
 
-function App() {
+
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Login' element={<Login/>}/>
+          <Route path='/Cadastro' element={<Cadastro/>}/>
+          <Route path='/EsqueceuSenha' element={<EsqueceuSenha/>}/>
+          <Route path='/Logou' element={<Logou/>}/>
+          <Route path='/Criadores' element={<Criadores/>}/>
+          <Route path='/Sobre' element={<Sobre/>}/>
+          <Route path='/BlogSemLogar' element={<BlogSemLogar/>}/>
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,51 @@
+import '../styles/index.css'
+import { Link } from 'react-router-dom'
+import blog from './images/blog.png';
+import pigup from './images/pigup.gif'
+
+export const Home = ():JSX.Element => {
+    return (
+        <div className='home-div'>
+            <header className='header'>
+                <div className='div-header-div'>
+                    <div className='Group-7'>
+                    <span><Link to='/Contato'>Contato</Link></span>
+                    <span><Link to='/Sobre'>Sobre</Link></span>
+                    <span><Link to='/Criadores'>Criadores</Link></span>
+                    </div>
+                    <div className='div-header-header'>
+                        <Link to='/Login' id='entrar'>Entrar</Link>
+                        <Link to='/Cadastro' id='cadastro'>Cadastro</Link>
+                    </div>
+                </div>
+            </header>
+            <img  alt="pigup" id='Group-8' src={pigup}/>
+            <div className='Group-6'>
+                <h1>CampaignVault</h1>
+                <span>Uma ferramenta que promete te deixar rico.</span>
+            </div>
+            <div className='cards'>
+                <div>
+                    <legend>Campanhas Efetivas</legend>
+                    <span>Descubra como transformar o retorno dos seus impulsionamentos em publicidade.</span>
+                </div>
+                <div>
+                    <legend>Dados e Analises Estrat.</legend>
+                    <span>Explore a importância da análise de dados para refinamentos contínuos das campanhas.</span>
+                    </div>
+                <div>
+                    <legend>Melhorias e ajustes</legend>
+                    <span>Saiba como ajustar suas estratégias para alcançar os melhores resultados em suas campanhas.</span>
+                </div>
+            </div>
+
+            
+            <div className='Blog'>
+
+                <Link to='/BlogSemLogar'><img alt="blog" id='blog' src={blog}/></Link>
+            </div>
+
+
+        </div>
+    ) 
+}
